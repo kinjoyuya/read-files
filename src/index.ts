8af01e8c7ab3d0e4e74570a0675ff80394bf487c
yan <kinjou_yuya@ca-adv.co.jp>
2020-04-14 17:15:35 +0900

    chore: pathとtypeを渡せば、ファイルかフォルダを返す関数追加

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('hello');
+import { promises } from 'fs';
+
+const getData = async (path: string, type: 'file' | 'folder'): Promise<string[]> => {
+  const foldersOrFiles = await promises.readdir(path, { withFileTypes: true });
+  return foldersOrFiles
+    .filter((folderOrFile) => (type === 'file' ? folderOrFile.isFile() : folderOrFile.isDirectory()))
+    .map((data) => data.name);
+};
