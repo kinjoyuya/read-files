5b708338aedf0d3b32ef28bf605ed7b820568147
yan <kinjou_yuya@ca-adv.co.jp>
2020-04-14 17:49:53 +0900

    chore: 単純にrecursiveをコピペしてみる

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
-import { promises } from 'fs';
+const fs = require('fs');
+const path = require('path');
 
-const getData = async (path: string, type: 'file' | 'folder'): Promise<string[]> => {
-  const foldersOrFiles = await promises.readdir(path, { withFileTypes: true });
-  return foldersOrFiles
-    .filter((folderOrFile) => (type === 'file' ? folderOrFile.isFile() : folderOrFile.isDirectory()))
-    .map((data) => data.name);
-};
+module.exports = read;
+
+function read(root, filter, files, prefix) {
+  prefix = prefix || '';
+  files = files || [];
+  filter = filter || noDotFiles;
+
+  const dir = path.join(root, prefix);
+  if (!fs.existsSync(dir)) return files;
+  if (fs.statSync(dir).isDirectory())
+    fs.readdirSync(dir)
+      .filter(function (name, index) {
+        return filter(name, index, dir);
+      })
+      .forEach(function (name) {
+        read(root, filter, files, path.join(prefix, name));
+      });
+  else files.push(prefix);
+
+  return files;
+}
+
+function noDotFiles(x) {
+  return x[0] !== '.';
+}
