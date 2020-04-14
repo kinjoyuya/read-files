7fe7ad008df893895cdfaa262ae34b5459f3bb82
yan <kinjou_yuya@ca-adv.co.jp>
2020-04-14 18:22:16 +0900

    fix: 型と初期値を設定

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ const noDotFiles = (x: string): boolean => {
   return x[0] !== '.';
 };
 
+type Filter = (path: string, index?: number, dir?: string) => boolean;
+
+export const read = (root: string, filter: Filter = noDotFiles, files: string[] = [], prefix = ''): string[] => {
   const dir = path.join(root, prefix);
   if (!fs.existsSync(dir)) return files;
   if (fs.statSync(dir).isDirectory())
