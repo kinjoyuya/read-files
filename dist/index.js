5e44c8072e04a630c12d57c4a2f0a44a55462c72
yuya <kinjo.bobchin@gmail.com>
2020-04-22 13:59:09 +0900

    fix: change build file's content

--- dist/index.js
+++ dist/index.js
@@ -2,22 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
 const path_1 = require("path");
-const noDotFiles = (x) => {
-    return x[0] !== '.';
-};
-exports.read = (root, filter = noDotFiles, files = [], prefix = '') => {
-    const dir = path_1.join(root, prefix);
-    if (!fs_1.existsSync(dir))
+exports.read = (root = './') => {
+    root = root || './';
+    const files = [];
+    if (!fs_1.existsSync(root))
         return files;
-    if (fs_1.statSync(dir).isDirectory())
-        fs_1.readdirSync(dir)
-            .filter((name, index) => {
-            return filter(name, index, dir);
-        })
-            .forEach((name) => {
-            exports.read(root, filter, files, path_1.join(prefix, name));
-        });
-    else
-        files.push(prefix);
+    const readRecursive = (path) => {
+        const data = fs_1.readdirSync(path_1.join(root, path), { withFileTypes: true });
+        data.filter((data) => data.isFile()).forEach((file) => files.push(path_1.join(path, file.name)));
+        data.filter((data) => data.isDirectory()).forEach((folder) => readRecursive(path_1.join(path, folder.name)));
+    };
+    readRecursive(root);
     return files;
 };
